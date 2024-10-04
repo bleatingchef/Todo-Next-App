254b243a05cfe4e11d72cd318d74b181ca3dca5f
bleatingchef <ananddurgesh701@gmail.com>
2024-10-04 22:38:48 +0530

    final changes

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='flex flex-wrap py-3 justify-around'>
-        <h1 className='text-lg font-semibold'>ToDo App</h1>
-        <ul className='flex gap-[40px] text-md'>
+        <h1 className='text-3xl font-semibold'>ToDo App</h1>
+        {/* <ul className='flex gap-[40px] text-md'>
             <li>Home</li>
             <li>Product</li>
             <li>About</li>
             <li>Contact</li>
-        </ul>
+        </ul> */}
 
     </div>
   )
